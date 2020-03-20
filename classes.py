43a2e5b800342b213771cf7c655c65a0d99786f7
Furkan <furkankocyigit58@gmail.com>
2020-03-20 08:56:57 -0700

    class.py codes are added

--- classes.py
+++ classes.py
@@ -42,3 +42,40 @@ print(s1.area1())
 
 s1.edge = 7
 print(s1.area1())
+
+# %% methods vs functions
+
+class Emp(object):
+    age = 25
+    salary = 1000
+    
+    def ageSalaryRatio(self):
+        print(self.age / self.salary)
+        
+def ageSalaryRatio(age, salary):
+    print(age / salary)
+
+       
+e1 = Emp()
+e1.ageSalaryRatio()
+
+# %% initializer or constructor
+
+class Animal(object):
+       
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def getAge(self):
+        return self.age
+    def getName (self):
+        print(self.name)
+
+
+a1 = Animal("dog", 2)
+a2 = Animal("cat", 4)
+a3 = Animal("bird",6)
+age = a1.getAge()
+
+print(age)
