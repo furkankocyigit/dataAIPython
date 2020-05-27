5ceef350c2b44dd108a5c6285e065af95a7a043a
furkankocyigit <furkan.kocyigit@avl.com>
2020-05-27 22:48:52 +0300

    pandas.py is added

--- AI for python/pandas.py
+++ AI for python/pandas.py
@@ -57,3 +57,54 @@ print(dataFrame1.loc[::-1])
 print(dataFrame1.loc[:,:"MAAS"])
 
 print(dataFrame1.iloc[:,2])
+
+# %% Filtering
+
+filtre1 = dataFrame1.MAAS > 200
+
+filtrelenmis_data = dataFrame1[filtre1]
+
+filtre2 = dataFrame1.AGE < 20
+dataFrame1[filtre1 & filtre2]
+
+# %% List Comprehension
+
+import numpy as np
+
+ortalama_maas = dataFrame1.MAAS.mean()
+
+ortalama_maas_np = np.mean(dataFrame1.MAAS)
+
+dataFrame1["maas_seviyesi"] = ["dusuk" if ortalama_maas > each else "yuksek" for each in dataFrame1.MAAS]
+
+dataFrame1.columns = [each.lower() for each in dataFrame1.columns]
+
+dataFrame1.columns = [ each.split()[0] + "_" + each.split()[1] if(len(each.split())>1) else each for each in dataFrame1.columns]
+
+# %% drop and concatenating
+
+dataFrame1.drop(["yeni_feature"],axis = 1,inplace = True)
+
+head = dataFrame1.head()
+tail = dataFrame1.tail()
+
+#vertical concatenate
+vert_data = pd.concat([head,tail],axis = 0)
+
+#horizontal
+
+maas = dataFrame1.maas
+age = dataFrame1.age
+
+data_h_concat = pd.concat([maas,age],axis = 1)
+
+# %% transforming data
+
+dataFrame1["list_comp"] = [each * 2 for each in dataFrame1.age]
+
+#apply()
+def multiply(age):
+    return age*2
+
+dataFrame1["multiply_metodu"] = [multiply(each) for each in dataFrame1.age]
+dataFrame1["apply_metodu"] = dataFrame1.age.apply(multiply)
